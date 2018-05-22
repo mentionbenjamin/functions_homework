9dd4755973e14f676713305ef04fa6181d7e7e25
Ben Robinson <mentionbenjamin@gmail.com>
2018-05-22 16:35:15 +0100

    calculations working

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,37 @@
+def return_10()
+  return 10
+end
+
+def add(number1, number2)
+  return(number1 + number2)
+end
+
+def subtract(number1, number2)
+  return(number1 - number2)
+end
+
+def multiply(number1, number2)
+  return(number1 * number2)
+end
+
+def divide(number1, number2)
+  return(number1 / number2)
+end
+
+# def length_of_string()
+#   return(string == 21)
+# end
+
+
+
+#
+# def test_length_of_string()
+#   test_string = "A string of length 21"
+#   length_of_string = length_of_string( test_string )
+#   assert_equal( 21, length_of_string )
+# end
+#
+# def test_divide()
+#   divide_result = divide( 10, 2 )
+#   assert_equal( 5, divide_result )
+# end
